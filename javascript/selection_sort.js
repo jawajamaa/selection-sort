297d18e51c2db096e243d76281591af2a3e8cd34
Timothy Ryon <tim@timryon.com>
2024-07-25 14:02:16 -0400

    js solution works and does not break with null/undefined, but needs refinement

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,23 +1,31 @@
 function selectionSort(arr) {
-  // type your code here
+  const compareNum = (a,b) => a - b;
+  return arr.filter(n => n !== 'undefined' || n !== null)
+  .sort(compareNum)
 }
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSort([3, -1, 5, 2]));
+// console.log(selectionSort([4, 1, 8, 3]))
+console.log(selectionSort([null, 4, 1, 8, 3, -1, undefined]))
 
-  console.log("");
+// if (require.main === module) {
+//   // add your own tests in here
+//   console.log("Expecting: [-1, 2, 3, 5]");
+//   console.log("=>", selectionSort([3, -1, 5, 2]));
 
-  // BENCHMARK HERE, and print the average runtime
-  const longInput = [];
+//   console.log("");
 
-  for (let i = 0; i < 100; ++i) {
-    longInput.push(Math.random());
-  }
-}
+//   // BENCHMARK HERE, and print the average runtime
+//   const longInput = [];
 
-module.exports = selectionSort;
+//   for (let i = 0; i < 100; ++i) {
+//     longInput.push(Math.random());
+//   }
+// }
 
-// Please add your pseudocode to this file
+// module.exports = selectionSort;
+
+// declare and initialize variable with sorting algo
+// remove any aberrant data, ie. undefined and null values
+// apply sorting algo and then return sorted struct
+
 // And a written explanation of your solution
